21841625125f4ef5bd7d6a777be700cc31a7dfba
tudornsa <ioneasa.tudor@gmail.com>
2017-12-15 22:20:25 +0200

    Denied direct access for login.php and logout.php

--- api/login.php
+++ api/login.php
@@ -1,4 +1,10 @@
 <?php
+    //check for direct access
+    if($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])){
+        //redirect
+        header('HTTP/1.0 403 Forbidden', TRUE, 403);
+        die(header('location: ../error.php'));
+    }
     session_start();
     
     if($_POST['username'] != "" && $_POST['password'] != ""){
