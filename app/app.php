37d2d701850fb70d8b0bc9900ce73c8403ec8560
tudornsa <ioneasa.tudor@gmail.com>
2017-12-15 21:48:18 +0200

    Denied direct access to app

--- app/app.php
+++ app/app.php
@@ -1,4 +1,14 @@
 <?php
-    echo "Hello I am app";
+    if($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])){
+        //redirect
+        header('HTTP/1.0 403 Forbidden', TRUE, 403);
+        die(header('location: ../error.php'));
+    }
+    session_start();
+
+    
+
+    echo "Hello I am app<br/>";
+    //TODO: Build app bellow.
     echo "<a href='api/logout.php'><input type='button' value='Log Out'/></a>"
 ?>
