b5e595393252c3bf5032506d17aa374439f94035
tudornsa <ioneasa.tudor@gmail.com>
2017-12-20 00:34:44 +0200

    signup.php works

--- api/signup.php
+++ api/signup.php
@@ -7,19 +7,30 @@
     }
     session_start();
 
-    if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'] && isset($_POST['password-c']))){
+    if(isset($_POST['username']) && $_POST['username'] !== "" && isset($_POST['email']) && $_POST['email'] !== "" && isset($_POST['password']) && $_POST['password'] !== "" && isset($_POST['password-c']) && $_POST['password-c'] !== ""){
         $username = $_POST['username'];
         $email = $_POST['email'];
         $password = $_POST['password'];
 
         if($password === $_POST['password-c']){
             //TODO:check for valid input and sanitize
+            $userFile = file_get_contents('../database/users.json');
+            if($userFile){
+                $data = array("username" => $username, "email" => $email, "password" => $password);
+
+                $usersJSON = json_decode($userFile);
+                array_push($usersJSON, $data);
+                file_put_contents('../database/users.json',json_encode($usersJSON));
+            }else{
+                $_SESSION['error'] = "Can't open users file";
+            }
         }else{
             $_SESSION['error'] = "Passwords don't match";
         }
     }else{
         $_SESSION['error'] = "Please fill all the fields!";
     }
+    header("Location: ../index.php");
 
 
 ?>
